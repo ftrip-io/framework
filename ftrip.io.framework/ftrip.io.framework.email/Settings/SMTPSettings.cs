1ae3c6aa641a521321f38b0124732616e5254807
Panic <panic.milos99@gmail.com>
2023-05-08 14:47:53 +0200

    feat/email-messaging-jwt-update (#16) (#17)
    feat: additional jwt configuration.
    feat: emailing framework.
    feat: one to many messaging.

---
+++ ftrip.io.framework/ftrip.io.framework.email/Settings/SMTPSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ftrip.io.framework.email.Settings
+{
+    public class SMTPSettings
+    {
+        public virtual string Host { get; set; }
+        public virtual int Port { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+    }
+}
--- ftrip.io.framework/ftrip.io.framework.messaging/Publisher/IMessagePublisher.cs
+++ ftrip.io.framework/ftrip.io.framework.messaging/Publisher/IMessagePublisher.cs
@@ -7,6 +7,6 @@ namespace ftrip.io.framework.messaging.Publisher
 {
     public interface IMessagePublisher
     {
-        Task Send<T, TId>(T message, CancellationToken cancellationToken = default) where T : IEvent<TId> where TId : IEquatable<TId>;
+        Task Send<T, TId>(T message, CancellationToken cancellationToken = default) where T : class, IEvent<TId> where TId : IEquatable<TId>;
     }
 }
