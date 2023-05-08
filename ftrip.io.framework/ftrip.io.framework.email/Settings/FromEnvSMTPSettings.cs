6910d6f51358c0d12a6a922a7c8eb828b81d050b
Panic <panic.milos99@gmail.com>
2023-05-08 14:47:33 +0200

    feat/email-messaging-jwt-update (#16)
    feat: additional jwt configuration.
    feat: emailing framework.
    feat: one to many messaging.

---
+++ ftrip.io.framework/ftrip.io.framework.email/Settings/FromEnvSMTPSettings.cs
@@ -0,0 +1,12 @@
+﻿using ftrip.io.framework.Utilities;
+
+namespace ftrip.io.framework.email.Settings
+{
+    public class FromEnvSMTPSettings : SMTPSettings
+    {
+        public override string Host { get => EnvReader.GetEnvVariableOrThrow("SMTP_HOST"); }
+        public override int Port { get => int.Parse(EnvReader.GetEnvVariableOrThrow("SMTP_PORT")); }
+        public override string Username { get => EnvReader.GetEnvVariableOrThrow("SMTP_USERNAME"); }
+        public override string Password { get => EnvReader.GetEnvVariableOrThrow("SMTP_PASSWORD"); }
+    }
+}
