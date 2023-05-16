cf7da2662f653499245e384bc73e2096e16d2813
Panic <panic.milos99@gmail.com>
2023-05-16 17:02:52 +0200

    Feat/logging physical delete (#21) (#22)
    * feat: serilog logging. physical delete mechanism.

---
+++ ftrip.io.framework/ftrip.io.framework/Logging/LoggingOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace ftrip.io.framework.Logging
+{
+    public class LoggingOptions
+    {
+        public string ApplicationName { get; set; }
+        public string ApplicationLabel { get; set; }
+        public string ClientIdAttribute { get; set; } = null;
+
+        public bool WriteToConsole { get; set; } = true;
+        public string ConsoleOutputTemplate { get; set; } = "[{Timestamp:yyyy-MM-dd HH:mm:ss} {ClientIp} {Level:u3}] - {Message:lj}{NewLine}";
+
+        public bool WriteToGrafanaLoki { get; set; } = true;
+        public string GrafanaLokiUrl { get; set; }
+    }
+}
