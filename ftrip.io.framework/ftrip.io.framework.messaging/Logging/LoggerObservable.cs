9772bf6489f4730047d5156fd2b5a8b3245e6c66
Panic <panic.milos99@gmail.com>
2023-05-17 19:44:00 +0200

    feat: added logging messages (#23)

---
+++ ftrip.io.framework/ftrip.io.framework.messaging/Logging/LoggerObservable.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace ftrip.io.framework.messaging.Logging
+{
+    public class LoggerObservable : IConsumeObserver
+    {
+        public Task ConsumeFault<T>(ConsumeContext<T> context, Exception exception) where T : class
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task PostConsume<T>(ConsumeContext<T> context) where T : class
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task PreConsume<T>(ConsumeContext<T> context) where T : class
+        {
+            Log.Logger.Information("Consuming message - Type[{Type}]", context.Message.GetType().Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- ftrip.io.framework/ftrip.io.framework/ExceptionHandling/Middleware/GlobalExceptionHandler.cs
+++ ftrip.io.framework/ftrip.io.framework/ExceptionHandling/Middleware/GlobalExceptionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using ftrip.io.framework.ExceptionHandling.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Linq;
+using Serilog;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -35,15 +36,25 @@ namespace ftrip.io.framework.ExceptionHandling.Middleware
         private Task HandleResponseBasedOnHandbleException(HttpContext context, HandlableException exception)
         {
             var httpStatusCode = HttpCodeMapper.From(exception);
+            var name = exception.GetType().Name;
+            var message = exception.Message;
 
-            return HandleResponse(context, httpStatusCode, exception.Message, exception.GetType().Name);
+            var logger = GetLogger(context);
+            logger.Error("Handable exception has occured - Exception[{Exception}], Message[{Message}]", name, message);
+
+            return HandleResponse(context, httpStatusCode, name, message);
         }
 
         private Task HandleResponseBasedOnUnexpectedException(HttpContext context, Exception exception)
         {
             var httpStatusCode = HttpStatusCode.InternalServerError;
+            var name = exception.GetType().Name;
+            var message = exception.Message;
 
-            return HandleResponse(context, httpStatusCode, exception.Message, exception.GetType().Name);
+            var logger = GetLogger(context);
+            logger.Error("Unexpected exception has occured - Exception[{Exception}], Message[{Message}]", name, message);
+
+            return HandleResponse(context, httpStatusCode, message, name);
         }
 
         private Task HandleResponse(HttpContext context, HttpStatusCode httpStatusCode, string message, string exceptionType)
@@ -59,6 +70,16 @@ namespace ftrip.io.framework.ExceptionHandling.Middleware
             });
 
             return context.Response.WriteAsync(responseObject.ToString());
+        }
+
+        private ILogger GetLogger(HttpContext context)
+        {
+            return GetService<ILogger>(context);
+        }
+
+        private T GetService<T>(HttpContext context)
+        {
+            return (T)context.RequestServices.GetService(typeof(T));
         }
     }
 }
