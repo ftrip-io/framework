17848b6ebc1097e6e7a742ef0f99db898f29c72b
Panic <panic.milos99@gmail.com>
2023-05-25 23:03:38 +0200

    feat: added tracing proxy interceptor

---
+++ ftrip.io.framework/ftrip.io.framework/Tracing/TracingProxyInterceptor.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+
+namespace ftrip.io.framework.Tracing
+{
+    public class TracingProxyInterceptor : IInterceptor
+    {
+        private readonly ITracer _tracer;
+
+        public TracingProxyInterceptor(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            using var activity = _tracer.ActivitySource.StartActivity($"{invocation.TargetType}.{invocation.Method.Name} called");
+
+            invocation.Proceed();
+        }
+    }
+}
