9e50edf90e547d959328d6f2ed0a8e262422c2aa
Panic <panic.milos99@gmail.com>
2023-06-26 19:51:01 +0200

    fix: populating contexts and filtering logs (#31) (#32)

--- ftrip.io.framework/ftrip.io.framework.messaging/Filters/PopulateLogContextFilter.cs
+++ ftrip.io.framework/ftrip.io.framework.messaging/Filters/PopulateLogContextFilter.cs
@@ -1,18 +1,30 @@
-﻿using GreenPipes;
+﻿using ftrip.io.framework.Contexts;
+using ftrip.io.framework.Correlation;
+using GreenPipes;
 using MassTransit;
 using Serilog.Context;
+using System;
 using System.Threading.Tasks;
 
 namespace ftrip.io.framework.messaging.Filters
 {
     public class PopulateLogContextFilter<T> : IFilter<ConsumeContext<T>> where T : class
     {
-        public PopulateLogContextFilter()
+        private readonly CurrentUserContext _currentUserContext;
+        private readonly CorrelationContext _correlationContext;
+
+        public PopulateLogContextFilter(
+            CurrentUserContext currentUserContext,
+            CorrelationContext correlationContext)
         {
+            _currentUserContext = currentUserContext;
+            _correlationContext = correlationContext;
         }
 
         public async Task Send(ConsumeContext<T> context, IPipe<ConsumeContext<T>> next)
         {
+            _currentUserContext.Id = "Consumer";
+            _correlationContext.Id = context.CorrelationId?.ToString() ?? Guid.NewGuid().ToString();
             using (LogContext.PushProperty("CorrelationId", context.CorrelationId))
             {
                 await next.Send(context);
